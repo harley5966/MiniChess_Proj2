6ee2282602e0630db6c64e6e5585d6e0e7d6ecf1
harley5966 <yuehling5966@gmail.com>
2026-06-17 09:48:11 +0800

    Add use_pvs parameter to MMParams struct

--- Proj2/src/policy/minimax.hpp
+++ Proj2/src/policy/minimax.hpp
@@ -6,12 +6,14 @@ struct MMParams {
     bool use_kp_eval = true;
     bool use_eval_mobility = true;
     bool report_partial = true;
+    bool use_pvs = true;
 
     static MMParams from_map(const ParamMap& m){
         MMParams p;
         p.use_kp_eval       = param_bool(m, "UseKPEval", true);
         p.use_eval_mobility = param_bool(m, "UseEvalMobility", true);
         p.report_partial    = param_bool(m, "ReportPartial", true);
+        p.use_pvs           = param_bool(m, "UsePVS", true);
         return p;
     }
 };
@@ -24,7 +26,9 @@ public:
         GameHistory& history,
         int ply,
         SearchContext& ctx,
-        const MMParams& p
+        const MMParams& p,
+        int alpha,
+        int beta
     );
     static SearchResult search(
         State *state,
